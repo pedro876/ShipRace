a9a151acda7cc160c84da74bd53a459217cf044a
pedro876 <pedrocasas876@gmail.com>
2022-06-26 12:28:25 +0200

    camera controller estabilizado

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    PlayerController target;
     Vector3 distanceToTarget;
     GyroFacade gyro;
     LevelManager level;
@@ -16,20 +16,22 @@ public class CameraController : MonoBehaviour
     {
         gyro = FindObjectOfType<GyroFacade>();
         level = FindObjectOfType<LevelManager>();
+        target = FindObjectOfType<PlayerController>();
     }
 
     private void Start()
     {
-        distanceToTarget = transform.position - target.position;
+        distanceToTarget = transform.position - target.transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        level.currentSection.rail.Project(transform.position, out var projPos, out var projRot);
+        //level.currentSection.rail.Project(transform.position, out var projPos, out var projRot);
+        Quaternion projRot = target.RailRotation;
         Vector3 camUp = Vector3.ProjectOnPlane(projRot * gyro.Attitude * Vector3.up, projRot * Vector3.forward);
         Quaternion targetRot = Quaternion.LookRotation(projRot * Vector3.forward, camUp);
-        Vector3 targetPos = target.position + projRot * distanceToTarget;
+        Vector3 targetPos = target.transform.position + projRot * distanceToTarget;
         transform.position = Vector3.Lerp(transform.position, targetPos, posLerp * Time.fixedDeltaTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, rotLerp * Time.fixedDeltaTime);
     }
